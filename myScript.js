a6429f0d38444d97cb27c5a5ec9900e50d5494cb
Bodrul-I <bodrul.islam93@googlemail.com>
2022-03-24 05:08:22 +0000

    Cloud-Resume-Project

---
+++ myScript.js
@@ -0,0 +1,22 @@
+// CUSTOM JS
+
+var btn = document.querySelector("button");
+btn.onclick = () => {
+ window.print()
+}
+
+// FONTAWESOME CHECK JS
+
+//  window.onload = function () {
+//   var span = document.createElement('span');
+
+//   span.className = 'fa';
+//   span.style.display = 'none';
+//   document.body.insertBefore(span, document.body.firstChild);
+
+//   alert(window.getComputedStyle(span, null).getPropertyValue('font-family'));
+
+//   document.body.removeChild(span);
+//  };
+
+
